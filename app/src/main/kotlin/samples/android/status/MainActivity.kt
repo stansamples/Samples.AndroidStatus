909ad48e23fbdd8feb7ea3d5b741a4b872b06fc4
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-09-24 13:59:55 +0300

    ram

--- app/src/main/kotlin/samples/android/status/MainActivity.kt
+++ app/src/main/kotlin/samples/android/status/MainActivity.kt
@@ -1,5 +1,6 @@
 package samples.android.status
 
+import android.app.ActivityManager
 import android.content.Context
 import android.os.Bundle
 import android.view.Gravity
@@ -16,6 +17,19 @@ internal class MainActivity : ComponentActivity() {
     private val providers = App.providers
     private val logger = providers.loggers.create("[Main]")
 
+    private fun updateStatus(context: Context, tv: TextView) {
+        val am = context.getSystemService(ActivityManager::class.java)
+        val info = ActivityManager.MemoryInfo()
+        am.getMemoryInfo(info)
+        tv.text = """
+            totalMem: ${info.totalMem} (${info.totalMem.toDouble().div(1_000_000).toLong()}mb)
+            availMem: ${info.availMem} (${info.availMem.toDouble().div(1_000_000).toLong()}mb)
+            occupied: ${info.totalMem - info.availMem} (${info.totalMem.toDouble().minus(info.availMem).div(1_000_000).toLong()}mb)
+            threshold: ${info.threshold} (${info.threshold.toDouble().div(1_000_000).toLong()}mb)
+            isLowMemory: ${info.lowMemory}
+        """.trimIndent()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val context: Context = this
@@ -26,6 +40,24 @@ internal class MainActivity : ComponentActivity() {
             )
             root.orientation = LinearLayout.VERTICAL
             root.gravity = Gravity.CENTER_VERTICAL
+            val statusText = TextView(context).also { view ->
+                view.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                )
+                root.addView(view)
+            }
+            Button(context).also { view ->
+                view.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                )
+                view.text = "update status"
+                view.setOnClickListener { _ ->
+                    updateStatus(context = context, statusText)
+                }
+                root.addView(view)
+            }
             val deviceOwnerTitle = TextView(context).also { view ->
                 view.layoutParams = ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.WRAP_CONTENT,
